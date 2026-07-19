7fe62f4d1bbf37699721ee981a51d7fa64bb1184
Bhavana-0207 <bhavanasree719@gmail.com>
2026-07-19 22:37:47 +0530

    Fix placement page

--- app/placement/page.tsx
+++ app/placement/page.tsx
@@ -1,8 +1,7 @@
-export default function
-PlacementPage() {
-    return (
-        <main className="min-h-screen flex items-center justify-center>"
-        <h1 className="text-3xl font-bold">Plaxement Hub</h1>
-        </main>
-    );
+export default function PlacementPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Placement Hub</h1>
+    </main>
+  );
 }
