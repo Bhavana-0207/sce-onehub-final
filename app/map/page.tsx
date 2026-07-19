0e28c7031508c49188c652bf8ca330998a79c4a5
Bhavana-0207 <bhavanasree719@gmail.com>
2026-07-19 21:50:43 +0530

    Use CampusMap component for map page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,5 @@
+import CampusMap from "@/components/CampusMap";
+
 export default function MapPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Campus Map</h1>
-    </main>
-  );
+  return <CampusMap />;
 }
